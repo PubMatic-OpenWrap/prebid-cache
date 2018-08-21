d365f4b4b42d9183470c60cbabedff48777e1b7d
Shalmali Patil <shalmali.patil@pubmatic.com>
2018-08-21 19:12:05 +0530

    disabling stats

--- stats/stats.go
+++ stats/stats.go
@@ -21,32 +21,32 @@ func InitStat(statIP, statPort, statServer, dc string) {
 
 func LogCacheFailedGetStats(errorString string) {
 	fmt.Printf(constant.StatsKeyCacheFailedGet, errorString)
-	S.Increment(fmt.Sprintf(constant.StatsKeyCacheFailedGet, errorString),
-		constant.StatsKeyCacheFailedGetCutoff, 1)
+	/*S.Increment(fmt.Sprintf(constant.StatsKeyCacheFailedGet, errorString),
+	constant.StatsKeyCacheFailedGetCutoff, 1)*/
 }
 
 func LogCacheMissStats() {
-	S.Increment(fmt.Sprintf(constant.StatsKeyCacheMiss),
-		constant.StatsKeyCacheMissCutOff, 1)
+	/*S.Increment(fmt.Sprintf(constant.StatsKeyCacheMiss),
+	constant.StatsKeyCacheMissCutOff, 1)*/
 }
 
 func LogCacheFailedPutStats(errorString string) {
-	S.Increment(fmt.Sprintf(constant.StatsKeyCacheFailedPut, errorString),
-		constant.StatsKeyCacheFailedPutCutoff, 1)
+	/*S.Increment(fmt.Sprintf(constant.StatsKeyCacheFailedPut, errorString),
+	constant.StatsKeyCacheFailedPutCutoff, 1)*/
 }
 
 func LogCacheRequestedGetStats() {
-	S.Increment(fmt.Sprintf(constant.StatsKeyCacheRequestedGet),
-		constant.StatsKeyCacheRequestedGetCutoff, 1)
+	/*S.Increment(fmt.Sprintf(constant.StatsKeyCacheRequestedGet),
+	constant.StatsKeyCacheRequestedGetCutoff, 1)*/
 }
 
 func LogCacheRequestedPutStats() {
-	S.Increment(fmt.Sprintf(constant.StatsKeyCacheRequestedPut),
-		constant.StatsKeyCacheRequestedPutCutoff, 1)
+	/*S.Increment(fmt.Sprintf(constant.StatsKeyCacheRequestedPut),
+	constant.StatsKeyCacheRequestedPutCutoff, 1)*/
 }
 
 func LogAerospikeErrorStats() {
-	S.Increment(fmt.Sprintf(constant.StatsKeyAerospikeCreationError),
-		constant.StatsKeyAerospikeCreationErrorCutoff, 1)
+	/*S.Increment(fmt.Sprintf(constant.StatsKeyAerospikeCreationError),
+	constant.StatsKeyAerospikeCreationErrorCutoff, 1)*/
 
 }
